4ccb7293ac9ee956ee0635a91fb3c61cfaf0cb92
danielesantos19 <daniele.c.santos19@gmail.com>
2023-01-30 21:26:59 -0300

    finalizando

---
+++ index.js
@@ -0,0 +1,7 @@
+function toggle(el){
+    let display = document.getElementById(el).style.display = 'block';
+    }
+function esconde(el){
+    const esconder = document.getElementById(el).style.display = 'none';
+
+}
